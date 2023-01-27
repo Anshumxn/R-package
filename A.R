9adad44baa4976c97965ad048fe6fcfe4fb411d2
Anshuman Dixit <77833758+Anshumxn@users.noreply.github.com>
2023-01-27 12:08:48 +0530

    Add files via upload

---
+++ A.R
@@ -0,0 +1,11 @@
+#' Basic Mathematics functions
+#' @export
+#' @param v numeric
+basic_function = function(v){
+  data.frame(
+    min = min(v),
+    max = max(v),
+    range = max(v)-min(v),
+    std_dev = sd(v)
+  )
+}
